448bd598b7b54b516a9ccf34757b4c7967536e10
JaneGe <625806758@qq.com>
2017-11-03 11:22:22 +0800

    add local files

---
+++ src/views/main.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="l-container">
+		<div class="l-header"></div>
+		<div class="l-body">
+			<div class="l-main">
+				<div class="profile"></div>
+				<div class="link-list">
+					<group>
+						<cell title="我的优惠券" is-link></cell>
+					</group>
+				</div>
+			</div>
+		</div>
+		<div class="l-footer"></div>
+	</div>
+</template>
+
+<script>
+	import { Cell, Group } from 'vux'
+	export default { components: { Cell, Group } }
+</script>
+
+<style>
+	.l-main .profile{ height:290px; }
+</style>
